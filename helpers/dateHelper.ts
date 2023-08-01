34869add5bce407cac854138388ecb143477bf10
Aly Hegazy <alyyasser19@gmail.com>
2023-08-01 20:15:29 +0300

    updated date

--- helpers/dateHelper.ts
+++ helpers/dateHelper.ts
@@ -1,5 +1,5 @@
 export const formatDate = (utcString: string): string => {
   const date = new Date(utcString);
   const options = { month: 'long', day: 'numeric', year: 'numeric' };
-  return date.toLocaleDateString('en-US', options);
+  return date.toLocaleDateString('en-US');
 };
